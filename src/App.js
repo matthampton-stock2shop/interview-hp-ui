5cbf25697a52958ce71a320754a8df8a1960bfea
Matt Hampton <matt.hampton@stock2shop.com>
2023-10-24 09:03:04 +0200

    Do a fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:3000/v1/products?sku=2")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <pre><code>{JSON.stringify(data, undefined, 2)}</code></pre>
+      </p>
     </div>
   );
 }
